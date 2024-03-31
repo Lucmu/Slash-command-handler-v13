5e4311dbfe083387e29c933b3a597390900fdb83
Lucmu <83866801+Lucmu@users.noreply.github.com>
2024-03-31 19:50:53 +0200

    Update ready.js

--- Eventos/ready.js
+++ Eventos/ready.js
@@ -1,3 +1,12 @@
+const Discord = require('discord.js');
+
 module.exports = async (client, interaction) => {
   console.log("Bot encendido con exito");
+  const loadslash = require("../slash.js")
+  try {
+      loadslash();
+      console.log("Cargando Nuevos Comandos");
+  } catch(err) {
+      console.log("No Hay Nuevos Comandos");
+  };
 };
