46630e977148ea4056517d1fa84e6cd7e997c750
Lucmu <83866801+Lucmu@users.noreply.github.com>
2024-03-31 19:47:47 +0200

    Create interactionCreate.js

---
+++ Eventos/interactionCreate.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+
+module.exports = async (client, interaction) => {
+  if (!interaction.isCommand()) return;
+  const slashcmd = client.slashcommands.get(interaction.commandName);
+  if (!slashcmd) return;
+  if (slashcmd) slashcmd.run(client, interaction);
+})
